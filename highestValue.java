c6b50557aa47f5cf38063d4723ba38f6e860fca7
sandygrs91 <124641369+sandygrs91@users.noreply.github.com>
2023-02-06 19:50:46 -0500

    Add files via upload

---
+++ highestValue.java
@@ -0,0 +1,23 @@
+public class highestValue {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 40;
+        int c = 80;
+
+        if (a > b && a > c) {
+            System.out.println("A will be the highest value" + a);}
+
+        else if  (b>a && b>c)
+
+          {System.out.println("Value B will be the greatest" + b);}
+
+
+         else
+
+              {System.out.println("value C will be Greatest :" + c);
+        }
+    }
+
+
+}
