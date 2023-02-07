fc81eb5235e71130ea47f1bde5cdd16795ae5866
sandygrs91 <124641369+sandygrs91@users.noreply.github.com>
2023-02-07 18:34:00 -0500

    Add files via upload

---
+++ LoopAssignment.java
@@ -0,0 +1,9 @@
+public class LoopAssignment {
+    public static void main(String[] args) {
+
+
+        for (int i =4 ; i <=30; i +=2)
+        {
+            System.out.println(" Even Numbers from 3 to 30 :" +i); }
+    }
+}
